aea6beaa7ab4561ee9d9ebf0e8b7aaa0efaaabb7
minhthanh1166 <buiminhthanh116@gmail.com>
2023-02-01 09:52:11 +0700

    first commit

---
+++ connectdb.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+  
+let db_con  = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '12345',
+    database: "testapi"
+});
+  
+db_con.connect((err) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("connected to Database");
+    }
+});
+  
+module.exports = db_con;
